2e7385ac4b46e9d916c67b280ee0dc99dc49a41b
darkiosys <darkiosys@gmail.com>
2019-05-07 02:24:51 +0700

    fix

--- app/Http/Controllers/ApiTopupController.php
+++ app/Http/Controllers/ApiTopupController.php
@@ -10,6 +10,8 @@ class ApiTopupController extends Controller
 	public function topupSaldo(Request $request)
 	{
 		$requestData = $request->all();
+		$requestData['created_at'] = date();
+		$requestData['updated_at'] = date();
 		$requestData['jumlah_transfer'] = $requestData['saldo'];
 		return Saldo::create($requestData);
 	}
