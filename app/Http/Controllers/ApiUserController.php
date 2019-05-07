16218008d4b534f6d563fd3a25e5092c733ed28b
darkiosys <darkiosys@gmail.com>
2019-05-07 13:31:31 +0700

    fix

--- app/Http/Controllers/ApiUserController.php
+++ app/Http/Controllers/ApiUserController.php
@@ -18,7 +18,8 @@ class ApiUserController extends Controller
 			"api_status" => 0,
 			"api_message" => "Invalid credentials. Check your username and password."
 		);
-		$user = User::where('email', '=', $requestData['email'])->first();
+		$user = User::where('email', '=', $requestData['email'])->orWhere('username', '=', $requestData['email'])->first();
+		return $user;
 		if($user){
 			$res = Hash::check($requestData['password'], $user->password);
 			if($res){
