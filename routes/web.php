8013cac1dc2f4e046fa5af100a5f7cc051bc6f35
darkiosys <darkiosys@gmail.com>
2019-10-12 18:06:41 +0700

    x

--- routes/web.php
+++ routes/web.php
@@ -85,3 +85,444 @@ Route::get('/api/totaltransaksi/pesananstoredikirim', 'ApiTransaksiController@to
 
 Route::get('/api/transaksi/sudahdibayar', 'ApiTransaksiController@invoiceSudahDibayar');
 Route::get('/api/transaksi/detailsudahdibayar', 'ApiTransaksiController@detailInvoiceSudahDibayar');
+
+
+Route::post('/member-edit', function () {
+
+    $members_id = Request::post('members_id');
+    $username   = Request::post('username');
+    $password   = Request::post('password');
+
+    $first_name         = Request::post('first_name');
+    $last_name          = Request::post('last_name');
+    $photo              = Request::post('photo');
+    $nik                = Request::post('nik');
+    $email              = Request::post('email');
+    $phone              = Request::post('phone');
+    $tgl_lahir          = Request::post('tgl_lahir');
+    $jenis_kelamin      = Request::post('jenis_kelamin');
+    
+    $fileName = null;
+    if (Request::hasFile('file')) {
+        $file = Request::file('file');
+        $fileName = md5($file->getClientOriginalName() . time()) . "." . $file->getClientOriginalExtension();
+        $file->move('./uploads/members/', $fileName);    
+    }
+    
+    $member = DB::select('SELECT id FROM members WHERE id=? AND username=? AND password=?', [$members_id,$username,$password]);
+    
+    // return '{
+    //         "api_message": "$first_name = '.$first_name.'\n$nik '.$nik.'\n$tgl_lahir = '.$tgl_lahir.'" 
+    //     }';
+
+    if(empty($member)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member belum terdaftar,\nsilahkan coba kembali!" 
+        }';
+    }
+    else if(empty($first_name)){
+        return '{
+            "api_status": 0,
+            "api_message": "Nama Depan tidak boleh kodong!" 
+        }';
+    }
+    
+    else if(empty($tgl_lahir)){
+        return '{
+            "api_status": 0,
+            "api_message": "Tgl Lahir tidak boleh kodong!" 
+        }';
+    }
+    
+    try{
+        
+        $affected = DB::update('UPDATE members 
+            SET first_name=?,last_name=?,photo=?,nik=?,email=?,phone=?,tgl_lahir=?,jenis_kelamin=?
+            WHERE id=?', 
+            [$first_name,$last_name,$photo,$nik,$email,$phone,$tgl_lahir,$jenis_kelamin,
+                            $members_id]);
+        
+         return '{
+                "api_status": 1,
+                "api_message": "Data Member berhasil diubah!" 
+            }';
+
+        // if( $affected >= 1){
+        //     return '{
+        //         "api_status": 1,
+        //         "api_message": "Data Member berhasil diubah!" 
+        //     }';
+        // }
+        // else{ return '{
+        //         "api_status": 0,
+        //         "api_message": "Data Member gagal diubah! " 
+        //     }'; }
+    } catch(TimeoutException $e) {
+           return '{
+                "api_status": 0,
+                "api_message": "Error = '.$e.'" 
+            }';
+            
+    } catch(Exception $e){
+        return '{
+            "api_status": 0,
+            "api_message": "Error = '.$e.'" 
+        }';
+    }
+});
+
+Route::post('/store-edit', function () {
+
+    $members_id = Request::post('members_id');
+    $username   = Request::post('username');
+    $password   = Request::post('password');
+    $condition   = Request::post('condition');
+    
+    $store_name         = Request::post('store_name');
+    $store_image        = Request::post('store_image');
+    $store_note         = Request::post('store_note');
+    $store_address      = Request::post('store_address');
+    $store_kode_pos     = Request::post('store_kode_pos');
+    $subdistrict_id     = Request::post('subdistrict_id');
+    $store_status       = Request::post('store_status');
+    
+    $member = DB::select('SELECT id FROM members WHERE id=? AND username=? AND password=?', [$members_id,$username,$password]);
+    
+    if(empty($member)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member belum terdaftar,\nsilahkan coba kembali!" 
+        }';
+    }
+    else if(empty($store_name)){
+        return '{
+            "api_status": 0,
+            "api_message": "Products ID tidak boleh kodong!" 
+        }';
+    }
+    
+    else if(empty($subdistrict_id)){
+        return '{
+            "api_status": 0,
+            "api_message": "Kecamatan tidak boleh kodong!" 
+        }';
+    }
+    
+    try{
+            $affected = 0;
+            if($condition == "Buat Toko"){
+                $affected = DB::update('UPDATE members 
+                    SET store_name=?,store_image=?,store_note=?,store_address=?,store_kode_pos=?,subdistrict_id=?,store_opened=?,store_status=?
+                    WHERE id=?', 
+                [$store_name,$store_image,$store_note,$store_address,$store_kode_pos,$subdistrict_id,date("YmdHis"),"Buka",
+                                    $members_id]);
+            }else{
+                $affected = DB::update('UPDATE members 
+                    SET store_name=?,store_image=?,store_note=?,store_address=?,store_kode_pos=?,subdistrict_id=?,store_status=?
+                    WHERE id=?', 
+                [$store_name,$store_image,$store_note,$store_address,$store_kode_pos,$subdistrict_id,$store_status,
+                                    $members_id]);
+            }
+            
+        if( $affected >= 1){
+            return '{
+                "api_status": 1,
+                "api_message": "Data toko berhasil disimpan!" 
+            }';
+        }
+        else{ return '{
+                "api_status": 0,
+                "api_message": "Data toko gagal disimpan!" 
+            }'; }
+    } catch(TimeoutException $e) {
+           return '{
+                "api_status": 0,
+                "api_message": "Error = '.$e.'" 
+            }';
+            
+    } catch(Exception $e){
+        return '{
+            "api_status": 0,
+            "api_message": "Error = '.$e.'" 
+        }';
+    }
+});
+
+Route::post('/carts-edit', function () {
+
+    $members_id = Request::post('members_id');
+    $username   = Request::post('username');
+    $password   = Request::post('password');
+    $edit_field   = Request::post('edit_field');
+    
+    $products_id    = Request::post('products_id');
+    $qty            = Request::post('qty');
+    $note           = Request::post('note');
+    $trx_date   = date("YmdHis");
+    
+    $member = DB::select('SELECT id FROM members WHERE id=? AND username=? AND password=?', [$members_id,$username,$password]);
+    
+    // return '{
+    //         "api_message": "$products_id = '.$products_id.'\n$qty '.$qty.'\n$note = '.$note.'" 
+    //     }';
+
+    if(empty($member)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member belum terdaftar,\nsilahkan coba kembali!" 
+        }';
+    }
+    else if(empty($products_id)){
+        return '{
+            "api_status": 0,
+            "api_message": "Products ID tidak boleh kodong!" 
+        }';
+    }
+    
+    try{
+        $affected = 0;
+        
+        switch ($edit_field){
+            case "qty":
+                 $affected = DB::update('UPDATE product_carts 
+                                SET qty = ?
+                                WHERE members_id=? AND products_id=?', 
+                                [$qty,$members_id,$products_id]);
+                break;
+            case "note":
+                $affected = DB::update('UPDATE product_carts 
+                                SET note=?
+                                WHERE members_id=? AND products_id=?', 
+                                [$note,$members_id,$products_id]);
+                break;
+            default: $affected = 0;
+            
+        }
+
+        if( $affected >= 1){
+            return '{
+                "api_status": 1,
+                "api_message": "Update cart berhasil!" 
+            }';
+        }
+        else{ return '{
+                "api_status": 0,
+                "api_message": "Update cart gagal, coba beberapa saat lagi!" 
+            }'; }
+    } catch(TimeoutException $e) {
+           return '{
+                "api_status": 0,
+                "api_message": "Error = '.$e.'" 
+            }';
+            
+    } catch(Exception $e){
+        return '{
+            "api_status": 0,
+            "api_message": "Error = '.$e.'" 
+        }';
+    }
+});
+
+Route::post('/tranfer-saldo', function () {
+
+    $members_id = Request::post('members_id');
+    $username   = Request::post('username');
+    $password   = Request::post('password');
+    
+    $usernameTujuan = Request::post('tujuan');
+    $nominal    = Request::post('nominal');
+    $trx_date   = date("YmdHis");
+    
+    $data_admin = DB::table('ppob_fee_admin')->select('fee_admin') ->where('ppob_slug', '=', 'transfer-saldo')->get();
+    $fee_admin = (int) $data_admin[0]->fee_admin;
+    
+    $memberTujuan = DB::select('SELECT id FROM members WHERE username=?', [$usernameTujuan]);
+    $member = DB::select('SELECT id,username,saldo,password FROM members WHERE id=?', [$members_id]);
+    $saldoMember = (int) $member[0]->saldo;
+    $hashedPassword = $member[0]->password;
+    
+    if(empty($member)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member belum terdaftar,\nsilahkan coba kembali!" 
+        }';
+    }
+    else if(empty($memberTujuan)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member tujuan belum terdaftar,\nsilahkan masukan username lainnya!" 
+        }';
+    }
+    else if($saldoMember < (int)($nominal + $fee_admin)){
+        return '{
+            "api_status": 0,
+            "api_message": "Saldo tidak cukup.\n\nSaat ini saldo anda :\nRp. '.number_format($saldoMember).'.\nSilahkan lakukan topup saldo!" 
+        }';
+    }
+    
+    if (!Hash::check($password, $hashedPassword)) {
+        if(md5($password) != $hashedPassword){
+            return '{
+                "api_status": 0,
+                "api_message": "Password salah, silahkan coba kembali!" 
+            }';
+        }
+    }
+
+        
+    try{
+        $affected = DB::update('UPDATE members AS mem_dari, members AS mem_tujuan 
+                                SET mem_dari.saldo=mem_dari.saldo-?, mem_tujuan.saldo=mem_tujuan.saldo+? 
+                                WHERE mem_dari.id=? AND mem_tujuan.username=?', 
+                                [($nominal+$fee_admin),$nominal,$members_id,$usernameTujuan,]);
+                    
+        
+        if( $affected >= 1){
+            $sender = DB::select('SELECT id,username,saldo FROM members WHERE id=?', [$members_id]);
+            $usernameSender =       $sender[0]->username;
+            $saldoSender    = (int) $sender[0]->saldo;
+            
+            $receiver = DB::select('SELECT id,username,saldo FROM members WHERE username=?', [$usernameTujuan]);
+            $idReceiver         = (int) $receiver[0]->id;
+            $usernameReceiver   =       $receiver[0]->username;
+            $saldoReceiver      = (int) $receiver[0]->saldo;
+
+            DB::table('t_transfer_saldo')->insert(
+                [
+                    'members_id' => $members_id, 'sender' => $usernameSender,'receiver' => $usernameReceiver,'nominal' => ($nominal+$fee_admin), 'ending_saldo' => $saldoSender,
+                     'date' => $trx_date,'status' => "Keluar",
+                ]
+            );
+            
+            DB::table('t_transfer_saldo')->insert(
+                [
+                    'members_id' => $idReceiver, 'sender' => $usernameSender,'receiver' => $usernameReceiver,'nominal' => $nominal, 'ending_saldo' => $saldoReceiver,
+                     'date' => $trx_date,'status' => "Masuk",
+                ]
+            );
+            
+            return '{
+                "api_status": 1,
+                "api_message": "Tranfer saldo berhasil!" 
+            }';
+        }
+        else{ return '{
+                "api_status": 0,
+                "api_message": "Tranfer saldo gagal, coba beberapa saat lagi!" 
+            }'; }
+    } catch(TimeoutException $e) {
+           return '{
+                "api_status": 0,
+                "api_message": "TimeoutException = '.$e.'" 
+            }';
+            
+    } catch(Exception $e){
+        return '{
+            "api_status": 0,
+            "api_message": "Error = '.$e.'" 
+        }';
+    }
+
+});
+Route::post('/transfer-saldo', function () {
+
+    $members_id = Request::post('members_id');
+    $username   = Request::post('username');
+    $password   = Request::post('password');
+    
+    $usernameTujuan = Request::post('tujuan');
+    $nominal    = Request::post('nominal');
+    $trx_date   = date("YmdHis");
+    
+    $data_admin = DB::table('ppob_fee_admin')->select('fee_admin') ->where('ppob_slug', '=', 'transfer-saldo')->get();
+    $fee_admin = (int) $data_admin[0]->fee_admin;
+    
+    $memberTujuan = DB::select('SELECT id FROM members WHERE username=?', [$usernameTujuan]);
+    $member = DB::select('SELECT id,username,saldo,password FROM members WHERE id=?', [$members_id]);
+    $saldoMember = (int) $member[0]->saldo;
+    $hashedPassword = $member[0]->password;
+    
+    if(empty($member)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member belum terdaftar,\nsilahkan coba kembali!" 
+        }';
+    }
+    else if(empty($memberTujuan)){
+        return '{
+            "api_status": 0,
+            "api_message": "Member tujuan belum terdaftar,\nsilahkan masukan username lainnya!" 
+        }';
+    }
+    else if($saldoMember < (int)($nominal + $fee_admin)){
+        return '{
+            "api_status": 0,
+            "api_message": "Saldo tidak cukup.\n\nSaat ini saldo anda :\nRp. '.number_format($saldoMember).'.\nSilahkan lakukan topup saldo!" 
+        }';
+    }
+    
+    if (!Hash::check($password, $hashedPassword)) {
+        if(md5($password) != $hashedPassword){
+            return '{
+                "api_status": 0,
+                "api_message": "Password salah, silahkan coba kembali!" 
+            }';
+        }
+    }
+
+        
+    try{
+        $affected = DB::update('UPDATE members AS mem_dari, members AS mem_tujuan 
+                                SET mem_dari.saldo=mem_dari.saldo-?, mem_tujuan.saldo=mem_tujuan.saldo+? 
+                                WHERE mem_dari.id=? AND mem_tujuan.username=?', 
+                                [($nominal+$fee_admin),$nominal,$members_id,$usernameTujuan,]);
+                    
+        
+        if( $affected >= 1){
+            $sender = DB::select('SELECT id,username,saldo FROM members WHERE id=?', [$members_id]);
+            $usernameSender =       $sender[0]->username;
+            $saldoSender    = (int) $sender[0]->saldo;
+            
+            $receiver = DB::select('SELECT id,username,saldo FROM members WHERE username=?', [$usernameTujuan]);
+            $idReceiver         = (int) $receiver[0]->id;
+            $usernameReceiver   =       $receiver[0]->username;
+            $saldoReceiver      = (int) $receiver[0]->saldo;
+
+            DB::table('t_transfer_saldo')->insert(
+                [
+                    'members_id' => $members_id, 'sender' => $usernameSender,'receiver' => $usernameReceiver,'nominal' => ($nominal+$fee_admin), 'ending_saldo' => $saldoSender,
+                     'date' => $trx_date,'status' => "Keluar",
+                ]
+            );
+            
+            DB::table('t_transfer_saldo')->insert(
+                [
+                    'members_id' => $idReceiver, 'sender' => $usernameSender,'receiver' => $usernameReceiver,'nominal' => $nominal, 'ending_saldo' => $saldoReceiver,
+                     'date' => $trx_date,'status' => "Masuk",
+                ]
+            );
+            
+            return '{
+                "api_status": 1,
+                "api_message": "Tranfer saldo berhasil!" 
+            }';
+        }
+        else{ return '{
+                "api_status": 0,
+                "api_message": "Tranfer saldo gagal, coba beberapa saat lagi!" 
+            }'; }
+    } catch(TimeoutException $e) {
+           return '{
+                "api_status": 0,
+                "api_message": "TimeoutException = '.$e.'" 
+            }';
+            
+    } catch(Exception $e){
+        return '{
+            "api_status": 0,
+            "api_message": "Error = '.$e.'" 
+        }';
+    }
+
+});
