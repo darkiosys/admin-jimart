e1df892ee793b7022b0352abe7408eb86d38ce9d
darkiosys <darkiosys@gmail.com>
2019-10-13 18:22:21 +0700

    x

--- app/User.php
+++ app/User.php
@@ -31,9 +31,9 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password',
-    ];
+    // protected $hidden = [
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be cast to native types.
