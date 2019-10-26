8752bdb4834d0341bc77233b8713dc1af1de4036
darkiosys <darkiosys@gmail.com>
2019-10-26 11:54:30 +0700

    x

--- app/Http/Controllers/ApiTopupController.php
+++ app/Http/Controllers/ApiTopupController.php
@@ -448,7 +448,6 @@ class ApiTopupController extends Controller
 		return $retdata;
 	}
 	function telkomPay(Request $request) {
-
 	}
 	function esamsatInqu(Request $request) {
 		$req = $request->all();
@@ -538,7 +537,6 @@ class ApiTopupController extends Controller
 		return $retdata;
 	}
 	function esamsatPay(Request $request) {
-
 	}
 	function hpascaInqu(Request $request) {
 		$req = $request->all();
@@ -628,13 +626,10 @@ class ApiTopupController extends Controller
 		return $retdata;
 	}
 	function hpascaPay(Request $request) {
-
 	}
 	function pdamInqu(Request $request) {
-
 	}
 	function pdamPay(Request $request) {
-
 	}
 	function callback(Request $request) {
 		$data = file_get_contents('php://input');
@@ -658,10 +653,10 @@ class ApiTopupController extends Controller
 			$trx = DB::table('t_ppob')->where('trx_id', '=', $o->data->ref_id)->update(['token'  => $token[0], 'status' => 'Gagal' ]);
 			$x = DB::select('SELECT * FROM t_ppob WHERE trx_id = "'.$o->data->ref_id.'"');
 			DB::update('UPDATE members SET saldo=saldo+? WHERE id=?', [$x[0]->total_tagihan, $x[0]->members_id]);
-			return "gagal";
+			return array("message" => "success");
 		} else {
 			$trx = DB::table('t_ppob')->where('trx_id', '=', $o->data->ref_id)->update(['token'  => $token[0] ]);
-			return "sukses";
+			return array("message" => "failed");
 		}
 	}
 	function plnPostpaidInquiry(Request $request) {
@@ -750,6 +745,225 @@ class ApiTopupController extends Controller
 		return $data;
 	}
 	function PostpaidPay(Request $request) {
+		$req = $request->all();
+		$username   = "089687271843";
+		// $apiKey = "7285d8726bcde318728";
+		$apiKey = "6845d79e9afc378c";
+		$tr_id  = $req['tr_id'];
+		$signature  = md5($username.$apiKey.$tr_id);
+		$json = '{	
+				"commands"		: "pay-pasca",
+				"username"		: "089687271843",
+				"tr_id"			: "'.$tr_id.'",
+				"sign"			: "'.$signature.'"
+				}';
+		$url = "https://testpostpaid.mobilepulsa.net/api/v1/bill/check";
+		// $url = "https://mobilepulsa.net/api/v1/bill/check";
+		$ch  = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		$rd = json_decode($data);
+		if($rd->data->status == 2){
+			return $data;
+		}
+		$ha = 3000 * (25/100);
+		$vshare = 3000 - $ha;
+		$v10 = $vshare * (10/100);
+		$v15 = $vshare * (15/100);
+		$v5 = $vshare * (5/100);
+		// CASHBACK
+		$cb = DB::update('UPDATE members SET saldo=saldo+? WHERE id=?', [$v10,$members_id]);
+		$pcb = array(
+			'reff_id' => $ref_id,
+			'username' => $member[0]->username,
+			'level' => 0,
+			'amount' => $v10
+		);
+		T_bonusgenerasi::create($pcb);
+		// LEVEL 1
+		$cm1 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$member[0]->sponsor]);
+		if(count($cm1) > 0) {
+			$cb1 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v15,$cm1[0]->username]);
+			$pbg1 = array(
+				'reff_id' => $ref_id,
+				'username' => $cm1[0]->username,
+				'level' => 1,
+				'amount' => $v15
+			);
+			T_bonusgenerasi::create($pbg1);
+			// LEVEL 2
+			$cm2 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm1[0]->sponsor]);
+			if(count($cm2) > 0) {
+				$cb2 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v15,$cm2[0]->username]);
+				$pbg2 = array(
+					'reff_id' => $ref_id,
+					'username' => $cm2[0]->username,
+					'level' => 2,
+					'amount' => $v15
+				);
+				T_bonusgenerasi::create($pbg2);
+				// LEVEL 3
+				$cm3 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm2[0]->sponsor]);
+				if(count($cm3) > 0) {
+					$cb3 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v10,$cm3[0]->username]);
+					$pbg3 = array(
+						'reff_id' => $ref_id,
+						'username' => $cm3[0]->username,
+						'level' => 3,
+						'amount' => $v10
+					);
+					T_bonusgenerasi::create($pbg3);
+					// LEVEL 4
+					$cm4 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm3[0]->sponsor]);
+					if(count($cm4) > 0) {
+						$cb4 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v10,$cm4[0]->username]);
+						$pbg4 = array(
+							'reff_id' => $ref_id,
+							'username' => $cm4[0]->username,
+							'level' => 4,
+							'amount' => $v10
+						);
+						T_bonusgenerasi::create($pbg4);
+						// LEVEL 5
+						$cm5 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm4[0]->sponsor]);
+						if(count($cm5) > 0) {
+							$cb5 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v10,$cm5[0]->username]);
+							$pbg5 = array(
+								'reff_id' => $ref_id,
+								'username' => $cm5[0]->username,
+								'level' => 5,
+								'amount' => $v10
+							);
+							T_bonusgenerasi::create($pbg5);
+							// LEVEL 6
+							$cm6 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm5[0]->sponsor]);
+							if(count($cm6) > 0) {
+								$cb6 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v10,$cm6[0]->username]);
+								$pbg6 = array(
+									'reff_id' => $ref_id,
+									'username' => $cm6[0]->username,
+									'level' => 6,
+									'amount' => $v10
+								);
+								T_bonusgenerasi::create($pbg6);
+								// LEVEL 7
+								$cm7 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm6[0]->sponsor]);
+								if(count($cm7) > 0) {
+									$cb7 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v5,$cm7[0]->username]);
+									$pbg7 = array(
+										'reff_id' => $ref_id,
+										'username' => $cm7[0]->username,
+										'level' => 7,
+										'amount' => $v5
+									);
+									T_bonusgenerasi::create($pbg7);
+									// LEVEL 8
+									$cm8 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm7[0]->sponsor]);
+									if(count($cm8) > 0) {
+										$cb8 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v5,$cm8[0]->username]);
+										$pbg8 = array(
+											'reff_id' => $ref_id,
+											'username' => $cm8[0]->username,
+											'level' => 8,
+											'amount' => $v10
+										);
+										T_bonusgenerasi::create($pbg8);
+										// LEVEL 9
+										$cm9 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm8[0]->sponsor]);
+										if(count($cm9) > 0) {
+											$cb9 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v5,$cm9[0]->username]);
+											$pbg9 = array(
+												'reff_id' => $ref_id,
+												'username' => $cm9[0]->username,
+												'level' => 9,
+												'amount' => $v5
+											);
+											T_bonusgenerasi::create($pbg9);
+											// LEVEL 10
+											$cm10 = DB::select('SELECT username,sponsor FROM members WHERE username=?', [$cm9[0]->sponsor]);
+											if(count($cm10) > 0) {
+												$cb10 = DB::update('UPDATE members SET saldo=saldo+? WHERE username=?', [$v5,$cm10[0]->username]);
+												$pbg10 = array(
+													'reff_id' => $ref_id,
+													'username' => $cm10[0]->username,
+													'level' => 10,
+													'amount' => $v5
+												);
+												T_bonusgenerasi::create($pbg10);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		$ns = $member[0]->saldo - $mp;
+		$xusr = User::where('id', '=', $member[0]->id)->first();
+		$xusr->update(array('saldo' => $ns));
+		$trxname = 'Postpaid';
+		if(isset($req['trxname'])) {
+			$trxname = $req['trxname'];
+		}
+		DB::table('t_ppob')->insert(
+			[
+				'members_id' => $members_id,
+				'trx_id' => $ref_id,
+				'trx_date' => Date('Y-m-d H:i:s'),
+				'trx_name' => $trxname,
+				'no_hp' => $req['hp'],
+				'tagihan' => $lamount[$req['code']][1], 
+				'fee_admin' => 1000,
+				'total_tagihan' => 0,
+				'ending_saldo' => $ns,
+				'product_code' => $code,
+				'status' => "Berhasil",
+				'status_bonus' => '0'
+			]
+		);
+		return $data;
+	}
+	function PostpaidInquiryDev(Request $request) {
+		$req = $request->all();
+		$username   = "089687271843";
+		// $apiKey = "7285d8726bcde318728";
+		$apiKey = "6845d79e9afc378c";
+		$ref_id  = uniqid('');
+		$month = "";
+		if(isset($req['month'])){
+			$month = $req['month'];
+		}
+		$signature  = md5($username.$apiKey.$ref_id);
+		$json = '{
+				"commands"    : "inq-pasca",
+				"username"    : "089687271843",
+				"ref_id"      : "'.$ref_id.'",
+				"hp"          : "'.$req['hp'].'",
+				"code"  	  : "'.$req['code'].'",
+				"sign"        : "'.md5($username.$apiKey.$ref_id).'",
+				"month"		  : "'.$month.'"
+				}';
+		$url = "https://testpostpaid.mobilepulsa.net/api/v1/bill/check";
+		// $url = "https://mobilepulsa.net/api/v1/bill/check";
+		$ch  = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+	function PostpaidPayDev(Request $request) {
 		$req = $request->all();
 		$username   = "089687271843";
 		// $apiKey = "7285d8726bcde318728";
@@ -1128,7 +1342,6 @@ class ApiTopupController extends Controller
 			"smartdataVOL60" => array(59100, 59100)
 		);
 		$actualprice = $lamount[$req['code']][1] + 1000;
-
 		if($member[0]->saldo < $actualprice) {
 			$tp = array(
 				'member_id' => $members_id,
