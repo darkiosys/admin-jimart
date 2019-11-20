4b0d65eb45d57fec3acb9a83ffa302aade313116
darkiosys <darkiosys@gmail.com>
2019-11-20 10:38:19 +0700

    x

--- app/Http/Controllers/IDTController.php
+++ app/Http/Controllers/IDTController.php
@@ -16,7 +16,8 @@ class IDTController extends Controller
         $req = $request->all();
 		$signature  = md5('7-3dbf#n4rT');
 
-		$url = "http://36.79.180.2:62455/edc/devel/sim_mlm?EDC=LOGIN.SP1911199562.4de480f38b17e801fc9e22a0e44af051.EDC.18%23%2A217%23%2A205%23%2A148";
+		// $url = "http://36.79.180.2:62455/edc/devel/sim_mlm?EDC=LOGIN.SP1911199562.4de480f38b17e801fc9e22a0e44af051.EDC.18%23%2A217%23%2A205%23%2A148";
+		$url = "http://36.79.180.2:62455/edc/devel/sim_mlm/?EDC=LOGIN.SP1911199562.4de480f38b17e801fc9e22a0e44af051.EDC.18%23%2A217%23%2A205%23%2A148";
 		$ch  = curl_init();
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 		curl_setopt($ch, CURLOPT_URL, $url);
