8684c2c199f0f8d7de211b80a286f5a03bb9ef22
darkiosys <darkiosys@gmail.com>
2019-11-20 09:47:57 +0700

    x

--- app/Http/Controllers/IDTController.php
+++ app/Http/Controllers/IDTController.php
@@ -20,7 +20,7 @@ class IDTController extends Controller
 		$ch  = curl_init();
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POST, 0);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, null);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
