7cf8304480687d817562ae6d9c611840a69be5da
darkiosys <darkiosys@gmail.com>
2019-11-22 14:44:56 +0700

    x

--- app/Http/Controllers/IDTController.php
+++ app/Http/Controllers/IDTController.php
@@ -76,10 +76,10 @@ class IDTController extends Controller
 	function BookingTicket(Request $request) {
 		$req = $request->all();
 		$url = "http://36.79.180.2:62455/edc/devel/sim_mlm/?EDC=TIKET.AIRLINES.BOOKING.EDC.18%23%2A217%23%2A205%23%2A148.".$req['pin'].".".$req['session']."&FROM=".$req['from']."&TO=".$req['to']."&DATE=".$req['date']."&FLIGHT=".$req['flight']."&ADULT=".$req['adult']."&CHILD=".$req['child']."&INFANT=".$req['infant']."&EMAIL=".$req['email']."&PHONE=".$req['phone']."&PASSANGERNAME=".$req['passangername']."&DATEOFBIRTH=".$req['dateofbirth']."&BAGGAGEVOLUME=".$req['baggagevolume']."&PASSPORTNUMBER=".$req['passportnumber']."&PASSPORTEXPIRED=".$req['passportexpired'];
-		// return $url;
+		$encodedurl = rawurlencode($url);
 		$ch  = curl_init();
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_URL, $encodedurl);
 		// curl_setopt($ch, CURLOPT_POST, 0);
 		// curl_setopt($ch, CURLOPT_POSTFIELDS, null);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
